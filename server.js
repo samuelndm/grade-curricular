4f779cf2d6dd6f49ed66149a92b442b590c61002
samuelndm <samuelnmoura@gmail.com>
2020-01-02 22:48:17 -0300

    fixing server.js paths - 4

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/GradeCursos/dist/'));
+//app.use(express.static(__dirname + 'ds/GradeCursos/dist/'));
+app.use(express.static(`$/dist/$`));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/GradeCursos/dist/index.html'));
+//res.sendFile(path.join(__dirname+'/GradeCursos/dist/index.html'));
+res.sendFile(path.join(`$/dist/$/index.html`));
 });
 
 // Start the app by listening on the default Heroku port
