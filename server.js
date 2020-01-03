790057d00f3366d153de58aa1999bf4f14e37482
samuelndm <samuelnmoura@gmail.com>
2020-01-02 22:43:05 -0300

    fixing server.js paths - 3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/GradeCursos/dist'));
+app.use(express.static(__dirname + '/GradeCursos/dist/'));
 
 app.get('/*', function(req,res) {
     
