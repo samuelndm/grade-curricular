38f2e4002d5e15b1205dffa39f0542aff6ec2ce8
samuelndm <samuelnmoura@gmail.com>
2020-01-04 23:51:31 -0300

    fixed some path problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule }   from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FluxogComponent } from './fluxog/fluxog.component';
+import { NavComponent } from './grade-curricular/nav/nav.component';
+import { FluxogComponent } from './grade-curricular/fluxog/fluxog.component';
 import { FooterComponent } from './footer/footer.component';
 import { GradeCurricularComponent } from './grade-curricular/grade-curricular.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { CursosPesquisadosComponent } from './cursos/cursos-pesquisados/cursos-pesquisados.component';
-import { CursosPesquisadoComponent } from './cursos/cursos-pesquisado/cursos-pesquisado.component';
 import { FaculdadePesquisadaComponent } from './cursos/faculdade-pesquisada/faculdade-pesquisada.component';
 import { CursoPesquisadoComponent } from './cursos/curso-pesquisado/curso-pesquisado.component';
 
@@ -23,8 +21,6 @@ import { CursoPesquisadoComponent } from './cursos/curso-pesquisado/curso-pesqui
     FooterComponent,
     GradeCurricularComponent,
     CursosComponent,
-    CursosPesquisadosComponent,
-    CursosPesquisadoComponent,
     FaculdadePesquisadaComponent,
     CursoPesquisadoComponent
   ],
