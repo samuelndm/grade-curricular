fd206c11682799be5a7f241218de6d043dec8345
samuelndm <samuenmoura@gmail.com>
2020-01-31 18:36:22 -0300

    add angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule }   from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,6 +21,8 @@ import { FooterComponent } from './footer/footer.component';
     HttpClientModule,
     RouterModule,
     FormsModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule
 
   ],
   providers: [],
